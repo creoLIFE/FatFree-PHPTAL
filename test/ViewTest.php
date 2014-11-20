e9509ad33a162e063c3421ad547162789b3050ae
Miro <mirek.ratman@gmail.com>
2014-11-20 22:31:53 +0100

    gitignore and tests update

--- test/ViewTest.php
+++ test/ViewTest.php
@@ -7,28 +7,28 @@ require_once( __DIR__ . '/../FatFree/View/Phptalview.php');
 
 class PhptalViewTest extends PHPUnit_Framework_TestCase
 {
-	const DIR_TMP = '/tmp';
-	const DIR_DEST = '/dest';
-	const DIR_VIEW = '/view';
-	const TPL_ENCODING = 'UTF-8';
-	const TPL_EXT = 'php';
-	const TPL_LIFETIME = '10';
-	const FILE_VALID = '/view/valid.html';
+    const DIR_TMP = '/tmp';
+    const DIR_DEST = '/dest';
+    const DIR_VIEW = '/view';
+    const TPL_ENCODING = 'UTF-8';
+    const TPL_EXT = 'php';
+    const TPL_LIFETIME = '10';
+    const FILE_VALID = '/view/valid.html';
 
-	/**
-	 * @var view
-	 */
-	protected $view;
+    /**
+     * @var view
+     */
+    protected $view;
 
-	public function __construct()
-	{
+    public function __construct()
+    {
         $this->view = new \FatFree\View\Phptalview();
-	}
+    }
 
-	public function testRender()
-	{
-		$this->view->var1 = "Test var";
-		$this->assertNotNull( $this->view->render( __DIR__ . self::FILE_VALID ) );
-	}
+    public function testRender()
+    {
+        $this->view->var1 = "Test var";
+        $this->assertNotNull( $this->view->render( __DIR__ . self::FILE_VALID ) );
+    }
 
 }
